0aef9c757b55e8dbe2f9a0b2fffbcbf3e6d8435c
Byung Yoo <byung1231@gmail.com>
2019-12-27 02:30:06 -0500

    Major changes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,99 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/*
 function App() {
   return (
     <div id="title">
       PickOne-Web
     </div>
   );
+}*/
+
+class Count extends React.Component{
+
+  constructor(){
+    super();
+    this.state = { count: 3,
+                  enableIncrement: true,
+                  enableDecrement: true
+                };
+
+    this.maxCount = 20;
+    this.minCount = 0;
+  }
+
+  increment = () => {
+
+    let currCount = this.state.count;
+    currCount += 1;
+    this.setState({count:currCount});
+
+    if (currCount >= this.maxCount) {
+      this.setState({enableIncrement: false});
+    }
+    else{
+      this.setState({enableIncrement: true});
+    }
+
+    if (currCount > this.minCount) {
+      this.setState({enableDecrement: true});
+    }
+
+
+  }
+
+  decrement = () => {
+
+    let currCount = this.state.count;
+    currCount -= 1;
+    this.setState({count:currCount});
+
+    if (currCount <= this.minCount) {
+      this.setState({enableDecrement: false});
+    }
+    else{
+      this.setState({enableDecrement: true});
+    }
+
+    if (currCount < this.maxCount) {
+      this.setState({enableIncrement: true});
+    }
+  }
+
+  render(){
+    return(
+      <div>
+      <ul class="sideButtonsList">
+          <li><button type="button" class="sideButton" onClick={this.increment} disabled={!this.state.enableIncrement}>+</button></li>
+          <li id="lblCount">{this.state.count}</li>
+          <li><button type="button" class="sideButton" onClick={this.decrement} disabled={!this.state.enableDecrement}>-</button></li>
+        </ul>
+
+        <div class="clearer"></div>
+      </div>
+    )
+
+
+  }
+
 }
+/*
+function increment(){
+
+  return(
+    var count = document.getElementById('lblCount').innerHTML;
+
+    document.getElementById('lblCount').innerHTML = count + 1;
+
+  );
+
+}*/
+
+
 
 const Form = () => (
-  
+
     <form>
       <input type="text" class="textbox"></input><br/>
       <input type="text" class="textbox"></input>
@@ -22,4 +105,12 @@ const Form = () => (
 );
 
 
-export { App, Form};
+const PickButton = () => (
+
+  <button type="button" class="pickButton">Pick One!</button>
+
+
+);
+
+
+export { Count, Form, PickButton};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {App, Form} from './App';
+import {Count, Form, PickButton} from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Count />, document.getElementById('sideButtons'));
 ReactDOM.render(<Form />, document.getElementById('form'));
+ReactDOM.render(<PickButton />, document.getElementById('pickButtonDiv'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
